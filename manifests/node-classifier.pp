e536b06af69d0cca57f8c7cf0f71685d03192613
Brian Fohl <brian.fohl@gmail.com>
2014-09-04 11:52:05 -0700

    latest changes

--- manifests/node-classifier.pp
+++ manifests/node-classifier.pp
@@ -1,14 +1,18 @@
 class node::classifier {
  
         $groupname = "$company_platform:$company_role"
+        notify{"In node classifier!": }
+        
         case $groupname {
                 "USA:Web" : {
+                        notify{"The platform is: ${groupName}": }
                         include roles::web
                 }
         }
  
         case $company_role {
                 "Application" : {
+                        notify{"The role is: ${$company_role}": }
                         include roles::application
                 }
         }      
--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,12 +1,12 @@
 node default
 {
-  
-  include apache
+  notify{"** DEFAULT NODE!! **": }
+  include node::classifier
 
 }
 
 node bb0cb2978133
 {
-  
+  notify{"** bb0cb2978133 NODE!! **": }
   include tomcat
 }
